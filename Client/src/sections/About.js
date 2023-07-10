b31aaf5020e5cfa9e3e5ed2893bba3a9e5b1255d
Eljowe <joelwickstroem@gmail.com>
2023-07-10 11:24:03 +0300

    Wording

--- Client/src/sections/About.js
+++ Client/src/sections/About.js
@@ -8,24 +8,20 @@ const About = () => {
                 <h1 className='title'>Foreword</h1>
                 <div className='Foreword'>
                     <p>
-                        This website was built and designed by me in its entirety. I am, however, not a graphic designer,
-                        as one might be able to observe. I am, in simple terms, an engineering-minded person doing projects I find interesting.
-                        That is also the sole reason this website exists, as I wanted to learn more about (frontend) web development and to implement and
-                        test the skills I had previously acquired. This website should see changes in the future as I keep learning about better practices, I am sure.
-                    </p>
-                    <p>
-                        Here one can find limited information about me in addition to article-like summarizations of some of the projects
-                        I have done. The topics of the projects found here vary substantially, ranging from machine learning to film photography.
+                        I am a 23-year-old Masters student currently specializing in Human-Computer Interaction at Aalto University.
+                        Alongside my primary field of study, I have also pursued a minor in Computer Science, further expanding my technical expertise. 
+                        My background is in electrical engineering, in which I hold a bachelor's degree from Aalto University.
+                        Additionally, I have a keen interest in Mathematics, which I try to study at University of Helsinki whenever time permits. 
+                        My academic journey is driven by an interest for exploring the intersection of technology and design.
                     </p>
                 </div>
             </div>
             <div className='Aboutmediv'>
                 <img src={profilepicture} alt="me" className='profilepic'/>
-                <h3 >About me</h3>
+                <h3 >About this website</h3>
                 <p className='Aboutme'>
-                    I am a 23-year-old masters student studying human-computer interaction at Aalto University. My minor is in computer science.
-                    My background is in electrical engineering in which I possess a bachelor's degree from Aalto.
-                    I also study mathematics at the University of Helsinki whenever I have time.
+                    I try to update this website from time to time, as I gather new experience through work and personal projects.
+                    This website serves as a portfolio, CV, and a playground for my front-end experiments.
                 </p>
             </div>
         </div>
